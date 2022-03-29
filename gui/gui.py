21b8306b414b9f423624f19d0dbbc2fe9fc7a286
dylan <dcjohnson@gmail.com>
2022-03-28 19:13:18 -0500

    Add a loading indicator. Show the selected folder.

--- gui/gui.py
+++ gui/gui.py
@@ -16,13 +16,11 @@ def callback(sender, app_data):
 
 def check_dir(sender, app_data):
     dirname = app_data['file_path_name']
-    if os.path.split(dirname)[-1] == 'docs':
-        print(f'{sender} is the correct directory')
-        print(f'Stamping files in directory {dirname}')
-
+    dpg.set_value("dirtext", f"{dirname} has been selected")
+        
 
 def stamp_files(sender, app_data):
-    print('Stamping docs now!')
+    dpg.add_loading_indicator(parent="Bates Stamp", pos=(150, 240))    
 
 
 def show_group(sender, app_data, user_data):
@@ -32,15 +30,16 @@ def show_group(sender, app_data, user_data):
         dpg.hide_item(user_data)
 
 
-dpg.add_file_dialog(directory_selector=True, show=False, callback=check_dir, tag="file_dialog_id")
 
-with dpg.window(label="Bates Stamp", width=800, height=300):
+with dpg.window(label="Bates Stamp", width=800, height=300, tag="Bates Stamp"):
+    dpg.add_file_dialog(directory_selector=True, show=False, callback=check_dir, tag="file_dialog_id")
     
     dpg.add_button(label="Directory Selector", callback=lambda: dpg.show_item("file_dialog_id"))
+    dpg.add_text('', tag="dirtext")
+
     dpg.add_input_text(label="Bates Prefix")
     
     with dpg.group(tag="position", show=False):
-        # TODO make these buttons visible only if the checkbox is ticked.
         xpos = dpg.add_input_int(label='x (int)', default_value=300)
         ypos = dpg.add_input_int(label='y (int)', default_value=30)
         rotation = dpg.add_input_int(label='rotation', default_value=0, max_value=360)
@@ -55,15 +54,18 @@ with dpg.window(label="Bates Stamp", width=800, height=300):
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 3*5)
             dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 3*3, 3*3)
     
+    
     dpg.add_button(label='Stamp!', callback=stamp_files, pos=(150, 200), tag='stamp')
     dpg.bind_item_theme('stamp', 'rounded')
     
+
 dpg.configure_app(manual_callback_management=True)
 
 dpg.create_viewport(title='Bates Stamp', width=800, height=600)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 # dpg.start_dearpygui()
+# Debugging
 while dpg.is_dearpygui_running():
     jobs = dpg.get_callback_queue()
     dpg.run_callbacks(jobs)
